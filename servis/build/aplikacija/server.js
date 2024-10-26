de4faaa7fe97a44fd3182d12ff82e554f68462a5
Marin Grabovac <grabovacmarin000@gmail.com>
2024-10-26 18:04:24 +0200

    #1 Implemented the right tsconfig and package.json

--- servis/build/aplikacija/server.js
+++ servis/build/aplikacija/server.js
@@ -1,17 +1,12 @@
 import express from "express";
 import path from 'path';
 import { fileURLToPath } from 'url';
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 const server = express();
-
 server.get("/dokumentacija", (zahtjev, odgovor) => {
     odgovor.sendFile(path.join(__dirname, "../../dokumentacija/dokumentacija.html"));
 });
-
-
 server.listen(3000, () => {
     console.log("Server is running on http://localhost:3000");
 });
